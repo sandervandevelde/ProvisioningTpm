c41d9b0d06da278be26baad5eec822f3df91b9be
Sander van de Velde <sander@vandevelde-online.com>
2018-12-23 14:47:26 +0100

    code added

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿namespace ProvisioningTpm
+{
+    // This example is based on:
+    // https://github.com/Azure-Samples/azure-iot-samples-csharp/tree/master/provisioning/Samples/device
+
+    using Microsoft.Azure.Devices.Provisioning.Client;
+    using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+    using Microsoft.Azure.Devices.Shared;
+    using System;
+    using Microsoft.Azure.Devices.Provisioning.Security;
+
+    public static class Program
+    {
+        // - pass Device Provisioning Service ID_Scope as a command-prompt argument
+        private static string _idScope = string.Empty;
+
+        // - pass an individual enrollment registration id for this device
+        private static string _registrationId = string.Empty;
+        private const string GlobalDeviceEndpoint = "global.azure-devices-provisioning.net";
+        
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Provision your TPM");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Usage: ProvisionTpm <IDScope> <RegistrationID>");
+            Console.WriteLine("Run this 'As Adminsitrator'");
+
+            if (string.IsNullOrWhiteSpace(_idScope) && (args.Length > 0))
+            {
+                _idScope = args[0];
+            }
+
+            if (string.IsNullOrWhiteSpace(_registrationId) && (args.Length > 1))
+            {
+                _registrationId = args[1];
+            }
+
+            if (string.IsNullOrWhiteSpace(_idScope)
+                    || string.IsNullOrWhiteSpace(_registrationId))
+            {
+                Console.WriteLine("Check if the parameters are corrent: ProvisionTpm <IDScope> <RegistrationID>");
+                return 1;
+            }
+
+            using (var security = new SecurityProviderTpmHsm(_registrationId))
+            using (var transport = new ProvisioningTransportHandlerAmqp(TransportFallbackType.TcpOnly))
+            {
+                // Note that the TPM simulator will create an NVChip file containing the simulated TPM state.
+                Console.WriteLine("Extracting endorsement key.");
+                string base64EK = Convert.ToBase64String(security.GetEndorsementKey());
+
+                Console.WriteLine(
+                    "In your Azure Device Provisioning Service please go to 'Manage enrollments' and select " +
+                    "'Individual Enrollments'. Select 'Add individual enrollment' then fill in the following:");
+
+                Console.WriteLine($"\tMechanism: TPM");
+                Console.WriteLine($"\tEndorsement key: {base64EK}");
+                Console.WriteLine($"\tRegistration ID: {_registrationId}");
+                Console.WriteLine($"\tSwitch over to the IoT Edge device enrollemnt is needed");
+                Console.WriteLine($"\tIoT Hub Device ID: {_registrationId} (or any other valid DeviceID)");
+                Console.WriteLine($"\tCheck if the correct IoT Hub is selected");
+                Console.WriteLine($"\tFinally, Save this individual enrollment");
+                Console.WriteLine();
+                Console.WriteLine("Press ENTER when ready. This will start finalizing the registration on your TPM");
+                Console.ReadLine();
+
+                ProvisioningDeviceClient provClient =
+                    ProvisioningDeviceClient.Create(GlobalDeviceEndpoint, _idScope, security, transport);
+
+                var client = new ProvisioningDeviceTpmClient(provClient, security);
+                client.RunTestAsync().GetAwaiter().GetResult();
+
+                Console.WriteLine("The registration is finalized on the TPM");
+                Console.WriteLine("The connection is tested by sending a test message");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ ProvisioningDeviceTpmClient.cs
@@ -0,0 +1,74 @@
+namespace ProvisioningTpm
+{
+    // This example is based on:
+    // https://github.com/Azure-Samples/azure-iot-samples-csharp/tree/master/provisioning/Samples/device
+
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Provisioning.Client;
+    using Microsoft.Azure.Devices.Shared;
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    public class ProvisioningDeviceTpmClient
+    {
+        ProvisioningDeviceClient _provClient;
+        SecurityProvider _security;
+
+        public ProvisioningDeviceTpmClient(ProvisioningDeviceClient provisioningDeviceClient, SecurityProvider security)
+        {
+            _provClient = provisioningDeviceClient;
+            _security = security;
+        }
+
+        public async Task RunTestAsync()
+        {
+            Console.WriteLine($"RegistrationID = {_security.GetRegistrationID()}");
+            VerifyRegistrationIdFormat(_security.GetRegistrationID());
+
+            Console.Write("ProvisioningClient RegisterAsync . . . ");
+            DeviceRegistrationResult result = await _provClient.RegisterAsync().ConfigureAwait(false);
+
+            Console.WriteLine($"{result.Status}");
+            Console.WriteLine($"ProvisioningClient AssignedHub: {result.AssignedHub}; DeviceID: {result.DeviceId}");
+
+            if (result.Status != ProvisioningRegistrationStatusType.Assigned) return;
+
+            IAuthenticationMethod auth;
+            if (_security is SecurityProviderTpm)
+            {
+                Console.WriteLine("Creating TPM DeviceClient authentication.");
+                auth = new DeviceAuthenticationWithTpm(result.DeviceId, _security as SecurityProviderTpm);
+            }
+            else if (_security is SecurityProviderX509)
+            {
+                Console.WriteLine("Creating X509 DeviceClient authentication.");
+                auth = new DeviceAuthenticationWithX509Certificate(result.DeviceId, (_security as SecurityProviderX509).GetAuthenticationCertificate());
+            }
+            else
+            {
+                throw new NotSupportedException("Unknown authentication type.");
+            }
+
+            using (DeviceClient iotClient = DeviceClient.Create(result.AssignedHub, auth, TransportType.Amqp))
+            {
+                Console.WriteLine("DeviceClient OpenAsync.");
+                await iotClient.OpenAsync().ConfigureAwait(false);
+                Console.WriteLine("DeviceClient SendEventAsync TpmTestMessage.");
+                await iotClient.SendEventAsync(new Message(Encoding.UTF8.GetBytes("TpmTestMessage"))).ConfigureAwait(false);
+                Console.WriteLine("DeviceClient CloseAsync.");
+                await iotClient.CloseAsync().ConfigureAwait(false);
+            }
+        }
+
+        private void VerifyRegistrationIdFormat(string v)
+        {
+            var r = new Regex("^[a-z0-9-]*$");
+            if (!r.IsMatch(v))
+            {
+                throw new FormatException("Invalid registrationId: The registration ID is alphanumeric, lowercase, and may contain hyphens");
+            }
+        }
+    }
+}
